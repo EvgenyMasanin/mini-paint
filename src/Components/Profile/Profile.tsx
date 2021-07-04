a21ae82a8b7a4ca3b27e5498a3cbcf017af20c24
MasaninE <gmasmin@gmail.com>
2021-07-04 13:47:32 +0300

    move logic from profile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,28 +1,13 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { useTypedSelector } from '../../Hooks/reduxHooks'
-import { getCurrentUserDrawings } from '../../Redux/UserData/userDataActions'
-import UserCardContainer from '../UserCard/UserCardContainer'
+import React from "react";
 
-const Profile = () => {
-
-    const dispatch = useDispatch()
-    const images = useTypedSelector(state => state.userData.images)
-
-    useEffect(() => {
-        dispatch(getCurrentUserDrawings())
-    }, [])
+const Profile: React.FC = props => {
 
     return (
         <div className="row flexGrow m0">
             <div className="col s12" ><h3>My drawings</h3></div>
             <div className="col s12 overflowY" style={{ maxHeight: 480 }}>
-
-                {images.length === 0 ? <h4 className='center'>You haven't drawn anything yet.</h4> : images.map(image => (
-                    <UserCardContainer key={image.id} image={image} />
-                ))}
+                {props.children}
             </div>
-
         </div>
     )
 }
--- src/Components/Profile/ProfileContainer.tsx
+++ src/Components/Profile/ProfileContainer.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useTypedSelector } from '../../Hooks/reduxHooks'
+import { getCurrentUserDrawings } from '../../Redux/UserData/userDataActions'
 import Profile from './Profile'
+import UserCardContainer from '../UserCard/UserCardContainer'
 
 const ProfileContainer = () => {
+
+    const dispatch = useDispatch()
+    const images = useTypedSelector(state => state.userData.images)
+
+    useEffect(() => {
+        dispatch(getCurrentUserDrawings())
+    }, [])
+
     return (
-        <Profile />
+        <Profile >
+            {images.length === 0 ? <h4 className='center'>You haven't drawn anything yet.</h4> : images.map(image => (
+                <UserCardContainer key={image.id} image={image} />
+            ))}
+        </Profile>
     )
 }
 
