eb295084924ac1deb6c11d47730ae096faf93369
MasaninE <gmasmin@gmail.com>
2021-07-04 13:39:16 +0300

    move logic from UserDrawings

--- src/Components/UserDrawings/UserDrawings.tsx
+++ src/Components/UserDrawings/UserDrawings.tsx
@@ -1,80 +1,24 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { useTypedSelector } from '../../Hooks/reduxHooks'
-import { getUserDrawings } from '../../Redux/UserDrawings/userDrawingsActions'
-import UserCardContainer from '../UserCard/UserCardContainer'
-import { useCollection } from "react-firebase-hooks/firestore";
-import { fireStore } from "../../Firebase/fireBase"
-import { getCurrentUserDrawings } from '../../Redux/UserData/userDataActions'
-import { useState } from 'react'
+import PreloaderCircle from '../Preloader/PreloaderCircle'
 import { SearchFormRedux } from './SearchForm'
 
-const UserDrawings = () => {
+interface IUserDrawings {
+    loading: boolean
+    onSubmit: any
+    handleDiscard: () => void
+}
 
-    const dispatch = useDispatch()
-    const [filter, setFilter] = useState('')
-    let images = useTypedSelector(state => state.userDrawings.images)
-    const [imagesToDisplay, setImagesToDisplay] = useState([] as typeof images)
-    useEffect(() => {
-        dispatch(getUserDrawings())
-        dispatch(getCurrentUserDrawings())
-    }, [])
-
-    useEffect(() => {
-        setImagesToDisplay(images)
-    }, [images])
-
-    useEffect(() => {
-        if (!filter) {
-            setImagesToDisplay(images)
-        }
-        else {
-            setImagesToDisplay(images.filter((image) => image.userName.startsWith(filter)))
-        }
-    }, [filter])
-
-    const [, loading] = useCollection(fireStore.collectionGroup("images"))
-
-    interface ISearchFormFields {
-        search: string
-    }
-    const onSubmit: any = (data: ISearchFormFields) => {
-        setFilter(data.search)
-    }
-
-    const handleDiscard = () => {
-        setFilter('')
-    }
-
+const UserDrawings: React.FC<IUserDrawings> = props => {
+    
     return (
         <div className="row flexGrow m0">
             <div className="col m12 l2 px1">
-                <SearchFormRedux onSubmit={onSubmit} handleDiscard={handleDiscard} />
+                <SearchFormRedux onSubmit={props.onSubmit} handleDiscard={props.handleDiscard} />
             </div>
-            {loading ?
-                <div className="col s12 center">
-                    <div className="preloader-wrapper big active">
-                        <div className="spinner-layer spinner-green-only">
-                            <div className="circle-clipper left">
-                                <div className="circle" />
-                            </div>
-                            <div className="gap-patch">
-                                <div className="circle" />
-                            </div>
-                            <div className="circle-clipper right">
-                                <div className="circle" />
-                            </div>
-                        </div>
-                    </div>
-                </div> :
+            {props.loading ?
+                <PreloaderCircle /> :
                 <div className="row col m12 l10 overflowY" >
-
-                    {imagesToDisplay.length === 0 && filter ? <h3>User: {filter} does not has images.</h3> :
-                        imagesToDisplay.map(image => (
-                            <UserCardContainer key={image.id} image={image} />
-                        ))}
+                    {props.children}
                 </div>}
-
         </div>
     )
 }
--- src/Components/UserDrawings/UserDrawingsContainer.tsx
+++ src/Components/UserDrawings/UserDrawingsContainer.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { useDispatch } from 'react-redux'
+import { fireStore } from '../../Firebase/fireBase'
+import { useTypedSelector } from '../../Hooks/reduxHooks'
+import { getUserDrawings, IUserDrawing } from '../../Redux/UserDrawings/userDrawingsActions'
+import UserCardContainer from '../UserCard/UserCardContainer'
 import UserDrawings from './UserDrawings'
 
 const UserDrawingsContainer = () => {
+
+    const dispatch = useDispatch()
+    const [filter, setFilter] = useState('')
+    let images = useTypedSelector(state => state.userDrawings.images)
+    const [imagesToDisplay, setImagesToDisplay] = useState([] as IUserDrawing[])
+    useEffect(() => {
+        dispatch(getUserDrawings())
+    }, [])
+
+    useEffect(() => {
+        setImagesToDisplay(images)
+    }, [images])
+
+    useEffect(() => {
+        if (!filter) {
+            setImagesToDisplay(images)
+        }
+        else {
+            setImagesToDisplay(images.filter((image) => image.userName.startsWith(filter)))
+        }
+    }, [filter])
+
+    const [, loading] = useCollection(fireStore.collectionGroup("images"))
+
+    interface ISearchFormFields {
+        search: string
+    }
+    const onSubmit: any = (data: ISearchFormFields) => {
+        setFilter(data.search)
+    }
+
+    const handleDiscard = () => {
+        setFilter('')
+    }
+
     return (
-        <UserDrawings />
+        <UserDrawings
+            handleDiscard={handleDiscard}
+            loading={loading}
+            onSubmit={onSubmit}
+        >
+            {imagesToDisplay.length === 0 && filter ? <h3>User: {filter} does not has images.</h3> :
+                imagesToDisplay.map((image: IUserDrawing) => (
+                    <UserCardContainer key={image.id} image={image} />
+                ))}
+        </UserDrawings>
     )
 }
 
